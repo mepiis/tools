a9012315ebedd667c447c34b4a0f23fab3c4901d
mepii <84114232+mepiis@users.noreply.github.com>
2025-07-04 10:19:47 +0900

    Create system-protection-enabled.cmd

---
+++ system-protection-enabled.cmd
@@ -0,0 +1,6 @@
+@echo off
+powershell -Command "Enable-ComputerRestore -Drive 'C:\'"
+powershell -Command "vssadmin Resize ShadowStorage /For=C: /On=C: /MaxSize=10GB"
+powershell -Command "Checkpoint-Computer -Description 'Manual Restore Point' -RestorePointType 'MODIFY_SETTINGS'"
+echo "Done."
+pause
